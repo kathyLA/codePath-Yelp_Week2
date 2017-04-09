05d3654ddb8e4366b3cac98ae5c0a3e690f45196
kathy yin <pei.hwa@icloud.com>
2017-04-08 17:21:40 -0700

    basic setting view controller set up

---
+++ Yelp/FilterViewController.swift
@@ -0,0 +1,58 @@
+//
+//  FilterViewController.swift
+//  Yelp
+//
+//  Created by kathy yin on 4/8/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    var categories: [[String: String]]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.estimatedRowHeight = 60
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        categories = []
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    @IBAction func onTapSearch(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func onTapCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SwitchCell") as? SwitchCell
+        return cell!
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
